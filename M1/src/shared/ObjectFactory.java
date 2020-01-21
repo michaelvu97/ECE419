9501ee94bb23479d6949be5643a7f634ed92b829
Michael Vu <michael.vu.97@outlook.com>
2020-01-21 18:07:10 -0500

    Implemented object factory

--- M1/src/shared/ObjectFactory.java
+++ M1/src/shared/ObjectFactory.java
@@ -8,15 +8,13 @@ public final class ObjectFactory {
 	 * Creates a KVClient object for auto-testing purposes
 	 */
     public static IKVClient createKVClientObject() {
-        // TODO Auto-generated method stub
-    	return null;
+        return new app_kvClient.KVClient();
     }
     
     /*
      * Creates a KVServer object for auto-testing purposes
      */
 	public static IKVServer createKVServerObject(int port, int cacheSize, String strategy) {
-		// TODO Auto-generated method stub
-		return null;
+		return new app_kvServer.KVServer(port, cacheSize, strategy);
 	}
 }
