18a7e09fe7c78b30dc28b564c5aeea82e0554578
Rigby  Jackson Charles Kenneth <jckrigby@gmail.com>
2020-01-26 14:43:27 -0500

    improve cache tests

--- M1/src/testing/CacheTest.java
+++ M1/src/testing/CacheTest.java
@@ -39,12 +39,12 @@ public class CacheTest extends TestCase {
 		cache.put(key5,value5);
 		cache.put(key6,value6);
 
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-		assertTrue(value5 == cache.get(key5));
-		assertTrue(value6 == cache.get(key6));
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2).equals(value2));
+		assertTrue(cache.get(key3).equals(value3));
+		assertTrue(cache.get(key4).equals(value4));
+		assertTrue(cache.get(key5).equals(value5));
+		assertTrue(cache.get(key6).equals(value6));
 	}
 
 	@Test
@@ -65,15 +65,27 @@ public class CacheTest extends TestCase {
 		cache.put(key2,value2);
 		cache.put(key3,value3);
 
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2).equals(value2));
+		assertTrue(cache.get(key3).equals(value3));
 
 		cache.delete(key2);
 
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(null == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2) == null);
+		assertTrue(cache.get(key3).equals(value3));
+		
+		cache.delete(key1);
+
+		assertTrue(cache.get(key1) == null);
+		assertTrue(cache.get(key2) == null);
+		assertTrue(cache.get(key3).equals(value3));
+
+		cache.delete(key3);
+
+		assertTrue(cache.get(key1) == null);
+		assertTrue(cache.get(key2) == null);
+		assertTrue(cache.get(key3) == null);
 	}
 
 	@Test
@@ -96,59 +108,32 @@ public class CacheTest extends TestCase {
 
 		cache.clear();
 
-		assertTrue(null == cache.get(key1));
-		assertTrue(null == cache.get(key2));
-		assertTrue(null == cache.get(key3));
+		assertTrue(cache.get(key1) == null);
+		assertTrue(cache.get(key2) == null);
+		assertTrue(cache.get(key3) == null);
 	}
 
 	@Test
 	public void testFIFO() {
 		_strategy = IKVServer.CacheStrategy.FIFO;
-		_cacheSize = 3;
+		_cacheSize = 10;
 		cache = new Cache(_cacheSize,_strategy);
 
 		Exception ex = null;
-		String key1 = "Key1";
-		String value1 = "Value1";
-		String key2 = "Key2";
-		String value2 = "Value2";
-		String key3 = "Key3";
-		String value3 = "Value3";
-		String key4 = "Key4";
-		String value4 = "Value4";
-		String key5 = "Key5";
-		String value5 = "Value5";
-		String key6 = "Key6";
-		String value6 = "Value6";
-
-		cache.put(key1,value1);
-		cache.put(key2,value2);
-		cache.put(key3,value3);
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-
-		cache.put(key4,value4);
-		assertTrue(null == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-
-		cache.put(key5,value5);
-		assertTrue(null == cache.get(key1));
-		assertTrue(null == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-		assertTrue(value5 == cache.get(key5));
-
-		cache.put(key6,value6);
-
-		assertTrue(null == cache.get(key1));
-		assertTrue(null == cache.get(key2));
-		assertTrue(null == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-		assertTrue(value5 == cache.get(key5));
-		assertTrue(value6 == cache.get(key6));
+		for(int i = 0; i < 200; i++){
+			String putKeyString = "key" + i;
+			String putValueString = "value" + i;
+			cache.put(putKeyString,putValueString);
+			for(int j = 0; j <= i; j++){
+				String getKeyString = "key" + j;
+				String getValueString = "value" + j;
+				if(j<=i-_cacheSize){
+					assertTrue(null == cache.get(getKeyString));
+				} else {
+					assertTrue(cache.get(getKeyString).equals(getValueString));
+				}
+			}
+		}
 	}
 	
 	@Test
@@ -174,33 +159,33 @@ public class CacheTest extends TestCase {
 		cache.put(key1,value1);
 		cache.put(key2,value2);
 		cache.put(key3,value3);
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2).equals(value2));
+		assertTrue(cache.get(key3).equals(value3));
 
 		cache.get(key1);
 		cache.put(key4,value4);
-		assertTrue(value1 == cache.get(key1));
+		assertTrue(cache.get(key1).equals(value1));
 		assertTrue(null == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
+		assertTrue(cache.get(key3).equals(value3));
+		assertTrue(cache.get(key4).equals(value4));
 
 		cache.put(key5,value5);
 		assertTrue(null == cache.get(key1));
 		assertTrue(null == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-		assertTrue(value5 == cache.get(key5));
+		assertTrue(cache.get(key3).equals(value3));
+		assertTrue(cache.get(key4).equals(value4));
+		assertTrue(cache.get(key5).equals(value5));
 
 		cache.get(key3);
 		cache.get(key4);
 		cache.put(key6,value6);
 		assertTrue(null == cache.get(key1));
 		assertTrue(null == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
+		assertTrue(cache.get(key3).equals(value3));
+		assertTrue(cache.get(key4).equals(value4));
 		assertTrue(null == cache.get(key5));
-		assertTrue(value6 == cache.get(key6));
+		assertTrue(cache.get(key6).equals(value6));
 	}
 
 	@Test
@@ -226,9 +211,10 @@ public class CacheTest extends TestCase {
 		cache.put(key1,value1);
 		cache.put(key2,value2);
 		cache.put(key3,value3);
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
-		assertTrue(value3 == cache.get(key3));
+
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2).equals(value2));
+		assertTrue(cache.get(key3).equals(value3));
 
 		cache.get(key1);
 		cache.get(key1);
@@ -242,10 +228,10 @@ public class CacheTest extends TestCase {
 		cache.get(key2);
 
 		cache.put(key4,value4);
-		assertTrue(value1 == cache.get(key1));
-		assertTrue(value2 == cache.get(key2));
+		assertTrue(cache.get(key1).equals(value1));
+		assertTrue(cache.get(key2).equals(value2));
 		assertTrue(null == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
+		assertTrue(cache.get(key4).equals(value4));
 
 		cache.get(key4);
 		cache.get(key4);
@@ -256,19 +242,18 @@ public class CacheTest extends TestCase {
 		cache.get(key1);
 
 		cache.put(key5,value5);
-		assertTrue(value1 == cache.get(key1));
+		assertTrue(cache.get(key1).equals(value1));
 		assertTrue(null == cache.get(key2));
 		assertTrue(null == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
-		assertTrue(value5 == cache.get(key5));
+		assertTrue(cache.get(key4).equals(value4));
+		assertTrue(cache.get(key5).equals(value5));
 
 		cache.put(key6,value6);
-
-		assertTrue(value1 == cache.get(key1));
+		assertTrue(cache.get(key1).equals(value1));
 		assertTrue(null == cache.get(key2));
 		assertTrue(null == cache.get(key3));
-		assertTrue(value4 == cache.get(key4));
+		assertTrue(cache.get(key4).equals(value4));
 		assertTrue(null == cache.get(key5));
-		assertTrue(value6 == cache.get(key6));
+		assertTrue(cache.get(key6).equals(value6));
 	}
 }
