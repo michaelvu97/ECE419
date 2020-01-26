8e9b05ed9c7f354fb9c11b6f768747d1d191c4e2
Michael Vu <michael.vu.97@outlook.com>
2020-01-26 18:23:15 -0500

    Left note in additional test

--- M1/src/testing/AdditionalTest.java
+++ M1/src/testing/AdditionalTest.java
@@ -6,10 +6,17 @@ import junit.framework.TestCase;
 
 public class AdditionalTest extends TestCase {
 	
-	// TODO add your test cases, at least 3
-	
 	@Test
 	public void testStub() {
+        /**
+         * PLEASE READ:
+         * Our additional tests are distributed across other test files;
+         *  - CacheTest
+         *  - DiskStorageTest
+         *  - KVMessageTest
+         *  - SerializationTest
+         *  - ServerStoreSmartTest
+         */
 		assertTrue(true);
 	}
 }
