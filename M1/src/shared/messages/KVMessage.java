aa7ffcc4008bbb664b89c8ed78696c01f219a836
Michael Vu <michael.vu.97@outlook.com>
2020-02-09 12:25:30 -0500

    Added new StatusTypes

--- M1/src/shared/messages/KVMessage.java
+++ M1/src/shared/messages/KVMessage.java
@@ -11,7 +11,10 @@ public interface KVMessage {
 		PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
 		PUT_ERROR, 		/* Put - request not successful */
 		DELETE_SUCCESS, /* Delete - request successful */
-		DELETE_ERROR; 	/* Delete - request successful */
+		DELETE_ERROR, 	/* Delete - request successful */
+		SERVER_STOPPED,         /* Server is stopped, no requests are processed */
+  		SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
+  		SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
 
 		public byte toByte(){
 			switch (this) {
