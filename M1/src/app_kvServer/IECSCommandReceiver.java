96c3bc1af114b15f959210227afdc50dc1d8e220
Michael Vu <michael.vu.97@outlook.com>
2020-02-29 11:45:49 -0500

    Added commands to KVAdminMessage

--- M1/src/app_kvServer/IECSCommandReceiver.java
+++ M1/src/app_kvServer/IECSCommandReceiver.java
@@ -4,5 +4,9 @@ package app_kvServer;
  * A connection which receieves commands from the ECS.
  */
 public interface IECSCommandReceiver {
-    
+
+    /**
+     * Establishes a connection to the ECS.
+     */
+    public void connect();
 }
