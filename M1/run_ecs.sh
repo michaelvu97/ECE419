0d591fa3456afa38832c1446c1c06a081f112bf2
Michael Vu <michaelvu@localhost.localdomain>
2020-03-17 16:53:18 -0400

    fixed script refs in run_Ecs

--- M1/run_ecs.sh
+++ M1/run_ecs.sh
@@ -2,7 +2,8 @@
 pkill java
 rm -rf ~/logs
 rm -rf ~/M1/logs
-./zookeeper stop
-./zookeeper start
-../zookeeper*/bin/zkCli.sh rmr /kvclients
+./zookeeper.sh stop
+./zookeeper.sh start
+../zookeeper*/bin/zkCli.sh deleteall /kvclients
 java -cp m2-ecs.jar app_kvECS.ui.Application ./src/app_kvECS/ecs.config $1
+./zookeeper.sh stop
